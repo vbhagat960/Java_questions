abaf0ba092e0a8c1ada7329bb904f4113043889f
vbhagat960 <80891836+vbhagat960@users.noreply.github.com>
2023-06-25 20:08:30 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,15 @@
+package GFG;
+import java.lang.Integer;
+public class compare {
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 20;
+		System.out.println(Integer.compare(a, b));
+		int x = 30;
+		int y = 20;
+		System.out.println(Integer.compare(x,y));
+		int l = 30;
+		int m = 30;
+		System.out.println(Integer.compare(l,m));
+	}
+}
